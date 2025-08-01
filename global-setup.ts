c726d028a187f350884de2fba67a39c8a80fa028
James Yap <contact@jamesyap.org>
2025-08-01 15:37:17 -0400

    fix: remove pointless browser launch

--- global-setup.ts
+++ global-setup.ts
@@ -1,14 +1,15 @@
-import { chromium, type FullConfig } from '@playwright/test';
+// import { chromium } from '@playwright/test';
+import { type FullConfig } from '@playwright/test';
 import path from 'path';
 import dotenv from 'dotenv';
 
 async function globalSetup(_: FullConfig) {
-  const browser = await chromium.launch({ headless: true });
+  // const browser = await chromium.launch({ headless: true });
 
   // Load environment variables from a .env file
   dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-  await browser.close();
+  // await browser.close();
 }
 
 export default globalSetup;
